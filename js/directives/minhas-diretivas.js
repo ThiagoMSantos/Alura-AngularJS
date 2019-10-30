04b9e2156fee2eeb6b6ca737e0d5937d06de135f
Thiago NSV <thiagomathias98@gmail.com>
2019-10-30 18:26:34 -0300

    Adicionando diretiva 'minhaFoto'.

--- js/directives/minhas-diretivas.js
+++ js/directives/minhas-diretivas.js
@@ -14,4 +14,20 @@ angular.module('minhasDiretivas', [])
 
 
     return ddo;
+})
+.directive('minhaFoto', function() {
+
+    var ddo = {};
+
+    ddo.restrict = "AE";
+
+    ddo.scope = {
+        titulo: '@',
+        url: '@'
+    };
+
+    ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">';           
+
+    return ddo;
+
 });

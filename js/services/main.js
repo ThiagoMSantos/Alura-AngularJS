837e10395d0083376bd7b4f911888505623e1b53
Thiago NSV <thiagomathias98@gmail.com>
2019-10-30 13:38:55 -0300

    Criando rotas responsáveis por INTRODUZIR as View's no index.html

--- js/services/main.js
+++ js/services/main.js
@@ -1,1 +1,17 @@
-angular.module('AluraPic', ['minhasDiretivas','ngAnimate']);
+angular.module('AluraPic', ['minhasDiretivas','ngAnimate','ngRoute'])
+.config(function($routeProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true); //Responsável por 'Não precisar do # na rota'.
+
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/fotos'}); // Responsável por redirecionar para '/fotos' caso não exista a rota digitada.
+
+    $routeProvider.when('/fotos/new', {
+        templateUrl: 'partials/foto.html'
+    });
+
+});

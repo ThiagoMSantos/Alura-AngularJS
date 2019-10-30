23dcca3798cdce8d2ac3b5049bcd46344d069025
Thiago NSV <thiagomathias98@gmail.com>
2019-10-29 22:31:58 -0300

    Adicionando 'scope' do filtro.

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -1,6 +1,25 @@
-angular.module('AluraPic').controller('FotosController', function($scope){
-    $scope.foto = {
-        titulo :  'Leão',
-        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+angular.module('AluraPic').controller('FotosController', function($scope, $http){
+    $scope.fotos = [];
+    $scope.filtro = "";
+
+    $http.get("v1/fotos")
+    .success(function(fotos){
+        $scope.fotos = fotos;
+    })
+    .error(function(error){
+        console.log("Erro ocorrido:  " , error);
+    });
+
+   /* //O HTTP EM SI FUNCIONA COMO UMA PROMESSA, EXEMPLO \/
+
+        $http.get("v1/fotos");
+        promise.then(function(retorno){
+            $scope.fotos = retorno.data;
+        }).catch(function(error){
+            console.log(error);
+        }); 
+
+    */
+
+
 });

79f8695844798f697c0283a353987becf7119d05
Thiago NSV <thiagomathias98@gmail.com>
2019-10-31 14:31:03 -0300

    Adicionando funções de remoção de foto

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -1,6 +1,7 @@
 angular.module('AluraPic').controller('FotosController', function($scope, $http){
     $scope.fotos = [];
     $scope.filtro = "";
+    $scope.mensagem = "";
 
     $http.get("v1/fotos")
     .success(function(fotos){
@@ -9,6 +10,18 @@ angular.module('AluraPic').controller('FotosController', function($scope, $http)
     .error(function(error){
         console.log("Erro ocorrido:  " , error);
     });
+
+    $scope.remover = function(foto){ //Remove a foto ao clicar no botão de REMOVER
+        $http.delete('v1/fotos/' + foto._id)
+        .success(function(){
+            var indiceFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceFoto, 1); // Responsável por deletar a foto do escopo, sem a necessidade de ralizar nova requisição get
+            $scope.mensagem = "Foto Removida!";
+        })
+        .error(function(erro){
+            $scope.mensagem = "Foto não Removida!";
+        });
+    };
 
    /* //O HTTP EM SI FUNCIONA COMO UMA PROMESSA, EXEMPLO \/
 

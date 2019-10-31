10a40aa7aff0bb8dcb6e1fe0b7d74676596d91cc
Thiago NSV <thiagomathias98@gmail.com>
2019-10-31 14:28:42 -0300

    Incluindo funções de alteração de foto

--- js/controllers/foto-controller.js
+++ js/controllers/foto-controller.js
@@ -1,17 +1,39 @@
-angular.module('AluraPic').controller('FotoController', function($scope, $http){
+angular.module('AluraPic').controller('FotoController', function($scope, $http, $routeParams){
     $scope.foto = {};
     $scope.mensagem = "";
 
+    if($routeParams.fotoId){
+        $http.get('v1/fotos/' + $routeParams.fotoId)
+        .success(function(foto){
+            console.log(foto)
+            $scope.foto = foto;
+        })
+        .error(function(erro){
+            console.log(erro);       
+        });
+    }
+
     $scope.submeter = function(){
-        if($scope.formulario.$valid){
-            $http.post('v1/fotos', $scope.foto)
-            .success(function(){
-                $scope.mensagem = "Foto cadastrada!";
-                $scope.foto = {};
-            })
-            .error(function(error){
-                $scope.mensagem = "Foto NÃO cadastrada!";
-            });
+        if($scope.formulario.$valid){ // Condição se vai alterar ou se vai criar
+            if($scope.foto._id){
+                $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                .success(function(){
+                    $scope.mensagem = "Foto alterada com suceso!";
+                })
+                .error(function(error){
+                    $scope.mensagem = "Foto NÃO alterada!";
+                });
+            }
+            else{
+                $http.post('v1/fotos', $scope.foto)
+                .success(function(){
+                    $scope.mensagem = "Foto cadastrada!";
+                    $scope.foto = {};
+                })
+                .error(function(error){
+                    $scope.mensagem = "Foto NÃO cadastrada!";
+                });
+            }
         }
     };
     
